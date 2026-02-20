eb8762bb41fbace00d868109d0a7fe78b36bed07
epavvlovv <mathis.pwl.pv@gmail.com>
2026-02-20 16:38:55 +0000

    Travail 20-02-2026

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+initial_height = 100
+boucing_factor = 0.6
+
+
+for i in range(10):
+    initial_height = initial_height * boucing_factor
+    print(round(initial_height,4))
+
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    month = month + 1
+    if extra_payment_start_month <= month <= extra_payment_end_month :
+        payment_true = payment + extra_payment
+    else :
+        payment_true = payment
+    if payment_true > principal :
+        total_paid = total_paid + (payment_true - principal)
+        principal = 0
+    else :
+        principal = principal * (1+rate/12) - (payment_true)
+        total_paid = total_paid + payment_true
+    print(month, total_paid, principal)
+
+print('Total paid', total_paid, 'over', month, 'months')
